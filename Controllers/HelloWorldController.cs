0336c8e930e167bee84563596ba80ba2feb6fa03
Liam Lim <liam.lim@gmail.com>
2022-04-15 15:49:44 +1000

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+       
+        // 
+        // GET: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action..";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/3?name=liam 
+        //public string Welcome(string name, int ID = 1)
+        //{
+            // return "This is the Welcome action method.";
+            // return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            // return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,2 @@
+Full tutorial and instruction for this solution is at:
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-6.0&tabs=visual-studio
